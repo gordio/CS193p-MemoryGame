fba83b891ac55c27269f0f2709a99e40a7a6f26d
Oleh Hordiienko <oleh.hordiienko@gmail.com>
2020-05-18 20:12:35 +0300

    Initial Commit

---
+++ CS193p-MemoryGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CS193p-MemoryGame
+//
+//  Created by Oleh Hordiienko on 2020-05-18.
+//  Copyright © 2020 R-Activity. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
