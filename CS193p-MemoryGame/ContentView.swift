31f38ab87e60463fa12b88941daf5c1d2ff92a4d
Oleh Hordiienko <oleh.hordiienko@gmail.com>
2020-05-19 13:38:26 +0300

    [Lesson-1] Implement basic SwiftUI card prototype

--- CS193p-MemoryGame/ContentView.swift
+++ CS193p-MemoryGame/ContentView.swift
@@ -8,11 +8,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+struct CardView: View {
+    var isFaceUp = false
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10).stroke(lineWidth: 2.0)
+                Text("🥶").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 10).fill(Color.secondary)
+            }
+        }
     }
 }
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            ForEach(0..<4) {_ in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(.secondary)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
